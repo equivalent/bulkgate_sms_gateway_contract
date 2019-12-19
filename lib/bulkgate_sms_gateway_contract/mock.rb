9d3e5dd330706e145bfdaafba709c5f14acf98cc
Tomas Valent <equivalent@eq8.eu>
2019-12-19 10:27:03 +0100

    mock to represent errors

--- lib/bulkgate_sms_gateway_contract/mock.rb
+++ lib/bulkgate_sms_gateway_contract/mock.rb
@@ -1,10 +1,10 @@
 module BulkgateSmsGatewayContract
   module Mock
     def self.send_sms(number:, body:, country:, sender_name: nil)
-      BulkgateSmsGatewayContract::InvalidPhoneNumber if number  == 'invalid_phone_number'
-      BulkgateSmsGatewayContract::InvalidPhoneNumber if country == 'invalid_phone_number'
-      BulkgateSmsGatewayContract::OutOfCredit if number  == 'low_credit'
-      BulkgateSmsGatewayContract::EmptyMessageBody if body.blank?
+      raise BulkgateSmsGatewayContract::InvalidPhoneNumber, 'invalid_phone_number' if number  == 'invalid_phone_number'
+      raise BulkgateSmsGatewayContract::InvalidPhoneNumber, 'invalid_phone_number' if country == 'invalid_phone_number'
+      raise BulkgateSmsGatewayContract::OutOfCredit, 'low_credit' if number  == 'low_credit'
+      raise BulkgateSmsGatewayContract::EmptyMessageBody, 'empty_message' if body.blank?
 
       msg = "BULKGATE: #{body} TO: '#{country}:#{number}'"
       msg +=  " SENDER: #{sender_name}" if sender_name
